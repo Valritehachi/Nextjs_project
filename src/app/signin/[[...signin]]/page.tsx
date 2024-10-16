dbfdc803b3d45ae720f595b001c1643c096ff272
MKDLAMINI <dlaminikhehla5@gmail.com>
2024-10-16 22:26:28 +0200

    added a timer function to 200 on the signin page so that it fades in to view dynamically after clicking 'signin'

--- src/app/signin/[[...signin]]/page.tsx
+++ src/app/signin/[[...signin]]/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
 import { SignIn } from "@clerk/nextjs";
+import { useState, useEffect } from "react";
 
 export default function SignInPage() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoaded(true), 200);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="flex justify-center items-center h-full">
+      <div
+        className={`transition-opacity duration-1000 ease-out ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        }`}
+      >
       <SignIn routing="path" path="/signin" signUpUrl="/signup" />
+      </div>
     </div>
   );
 }
