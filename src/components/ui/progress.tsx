0753f18a8062caae0e6bc53fc494fbbb60d290f0
MKDLAMINI <dlaminikhehla5@gmail.com>
2024-10-16 17:10:09 +0200

    added a smoother effect on the progress bar on the progress.tsx file

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
-
 import { cn } from "@/lib/utils";
 
 const Progress = React.forwardRef<
@@ -18,11 +17,14 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      className="h-full bg-primary transition-transform duration-500 ease-in-out"
+      style={{
+        transform: `translateX(-${100 - (value || 0)}%)`,
+      }}
     />
   </ProgressPrimitive.Root>
 ));
+
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
